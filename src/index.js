fa8d8a41caa02d2f4d591ef2719718b230a0ad67
Bijan Pradhananga <100182448+bijan-pradhananga@users.noreply.github.com>
2024-11-04 09:00:42 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(cors({
   credentials: true,
   origin: [
     'https://portfolio-client-gamma-plum.vercel.app',
-    'https://portfolio-project-form.vercel.app/',
+    'https://portfolio-project-form.vercel.app',
   ]
 }));
 
